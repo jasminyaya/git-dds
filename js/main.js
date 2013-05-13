c6ee0a3b8dafb3946a75d940ece18467a0433eea
Sanna Nilsson <sanna@outloud.se>
2013-05-13 10:36:39 +0200

    Taking away the js and linking to it

--- js/main.js
+++ js/main.js
@@ -1,7 +1,10 @@
 // Why hello there, sexy.
 // This is a javascript comment. Any line that begins with // will be like this.
 // Comments have NO effect on your webpage. NONE! ZERO!
+<<<<<<< HEAD
 <script type="text/javascript">
+=======
+>>>>>>> Taking away the js and linking to it
     window.onload = function() {
       var node = document.getElementById('blinking-text');
       setInterval(function() {
@@ -12,7 +15,11 @@
           node.style.visibility = "";
         }
       }, 800);
+<<<<<<< HEAD
     }
   </script>
 </body>
 </html>
+=======
+    }
+>>>>>>> Taking away the js and linking to it
